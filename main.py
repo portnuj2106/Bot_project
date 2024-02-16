b11956f0623e4bde94f4884f98408aa45c569c91
portnuj2106 <146323737+portnuj2106@users.noreply.github.com>
2024-02-16 21:30:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 
 import asyncio
-import html
 import logging
 from dataclasses import dataclass
 from http import HTTPStatus
 
 import uvicorn
 from asgiref.wsgi import WsgiToAsgi
-from flask import Flask, Response, abort, make_response, request
+from flask import Flask, Response, request
 
 import sqlite3
 from telegram import Update
-from telegram.constants import ParseMode
 from telegram.ext import (
     Application,
     CallbackContext,
     CommandHandler,
     ContextTypes,
     ExtBot,
-    TypeHandler,
     MessageHandler,
     filters
 )
@@ -34,13 +31,13 @@ logger = logging.getLogger(__name__)
 URL = "https://cd25-46-63-28-245.ngrok-free.app"
 ADMIN_CHAT_ID = 123456
 PORT = 8000
-TOKEN = "6975785463:AAH716QilWD2HXs7i3lXgE_DQPoKjkIgG78"  # nosec B105
+TOKEN = "6975785463:AAH716QilWD2HXs7i3lXgE_DQPoKjkIgG78"
 
 
 @dataclass
 class WebhookUpdate:
     user_id: int
-    payload: str
+
 class CustomContext(CallbackContext[ExtBot, dict, dict, dict]):
     @classmethod
     def from_update(
@@ -82,7 +79,6 @@ async def start(update: Update, context: CustomContext) -> None:
 
         await context.bot.send_message(chat_id=update.effective_chat.id, text="This user already exists")
 
-
 async def delete(update: Update, context: CustomContext) -> None:
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
@@ -94,40 +90,17 @@ async def delete(update: Update, context: CustomContext) -> None:
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-
-async def webhook_update(update: WebhookUpdate, context: CustomContext) -> None:
-    """Handle custom updates."""
-    chat_member = await context.bot.get_chat_member(chat_id=update.user_id, user_id=update.user_id)
-    payloads = context.user_data.setdefault("payloads", [])
-    payloads.append(update.payload)
-    combined_payloads = "</code>\n• <code>".join(payloads)
-    text = (
-        f"The user {chat_member.user.mention_html()} has sent a new payload. "
-        f"So far they have sent the following payloads: \n\n• <code>{combined_payloads}</code>"
-    )
-    await context.bot.send_message(chat_id=ADMIN_CHAT_ID, text=text, parse_mode=ParseMode.HTML)
-
-
 async def main() -> None:
-    """Set up PTB application and a web application for handling the incoming requests."""
     context_types = ContextTypes(context=CustomContext)
-    # Here we set updater to None because we want our custom webhook server to handle the updates
-    # and hence we don't need an Updater instance
     application = (
         Application.builder().token(TOKEN).updater(None).context_types(context_types).build()
     )
-
-
     # register handlers
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("delete", delete))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-    application.add_handler(TypeHandler(type=WebhookUpdate, callback=webhook_update))
-
-
     await application.bot.set_webhook(url=f"{URL}/telegram", allowed_updates=Update.ALL_TYPES)
-
 
     flask_app = Flask(__name__)
 
@@ -136,28 +109,6 @@ async def main() -> None:
         await application.update_queue.put(Update.de_json(data=request.json, bot=application.bot))
         return Response(status=HTTPStatus.OK)
 
-    @flask_app.route("/submitpayload", methods=["GET", "POST"])  # type: ignore[misc]
-    async def custom_updates() -> Response:
-
-        try:
-            user_id = int(request.args["user_id"])
-            payload = request.args["payload"]
-        except KeyError:
-            abort(
-                HTTPStatus.BAD_REQUEST,
-                "Please pass both `user_id` and `payload` as query parameters.",
-            )
-        except ValueError:
-            abort(HTTPStatus.BAD_REQUEST, "The `user_id` must be a string!")
-
-        await application.update_queue.put(WebhookUpdate(user_id=user_id, payload=payload))
-        return Response(status=HTTPStatus.OK)
-
-    @flask_app.get("/healthcheck")  # type: ignore[misc]
-    async def health() -> Response:
-        response = make_response("The bot is still running fine :)", HTTPStatus.OK)
-        response.mimetype = "text/plain"
-        return response
 
     @flask_app.route("/")
     async def index():
